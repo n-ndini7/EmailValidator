e67f24cb3b1f7d635fcde6a51090ab5999daf9b8
n-ndini7 <hp@RJs-PC>
2020-09-28 23:05:59 +0530

    [Nandini] added master - welcome to email validator

---
+++ src/com/capgemini/EmailValidator.java
@@ -0,0 +1,8 @@
+package com.capgemini;
+
+public class EmailValidator {
+//Master branch - welcome message
+	public static void main(String[] args) {
+		System.out.println("Welcome to Email Validator program!!!");
+	}
+}
